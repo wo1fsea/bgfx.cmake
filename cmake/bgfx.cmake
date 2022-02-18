03c56d5810ff6711f27f5c60af5895ab6485f61a
wo1fsea <wo1fsea@126.com>
2022-02-18 19:40:39 +0800

    bgfx cmake

--- cmake/bgfx.cmake
+++ cmake/bgfx.cmake
@@ -113,6 +113,10 @@ if( UNIX AND NOT APPLE AND NOT EMSCRIPTEN AND NOT ANDROID )
 	target_link_libraries( bgfx PUBLIC ${X11_LIBRARIES} ${OPENGL_LIBRARIES})
 endif()
 
+if( ANDROID )
+	target_link_libraries( bgfx PUBLIC EGL)
+endif()
+
 # Exclude mm files if not on OS X
 if( NOT APPLE )
 	set_source_files_properties( ${BGFX_DIR}/src/glcontext_eagl.mm PROPERTIES HEADER_FILE_ONLY ON )
